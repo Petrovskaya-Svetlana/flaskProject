b719632fd3020c811213e3f8186ceca5f8e0ac37
Petrovskaya-Svetlana <s7606003@gmail.com>
2023-09-12 12:57:41 +0300

    finished 141

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
 class Article(db.Model):
-    id = db.Colunm(db.Integer, primary_key=True)
-    title = db.Column(db.Sring(100), nullable=False)
-    intro = db.Column(db.Sring(300), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
     text = db.Column(db.Text, nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
 
@@ -29,9 +30,9 @@ def about():  # put application's code here
     return render_template('about.html')
 
 
-@app.route('/user/<string:name>/<int:id>')
-def user(name, id):  # put application's code here
-    return 'Hello, from user page ' + name + '-' + str(id)
+@app.route('/create')
+def create():  # put application's code here
+    return render_template('create.html')
 
 
 if __name__ == '__main__':
